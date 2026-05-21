276183e2de94f8b574c2aec08ed75b839b48b8de
Danish Fayyadh Altamis <78647141+danishfa@users.noreply.github.com>
2026-05-21 19:28:51 +0700

    fix 403 forbidden webform and filling telegram token and chat ID

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,9 @@ echo "=============================="
 echo "🚀 START DEPLOY (PROD)"
 echo "=============================="
 
+# ==============================
+# STEP 1 — TRIAGE
+# ==============================
 echo "=== STEP 1: DEPLOY TRIAGE ==="
 cd triage
 
@@ -17,7 +20,7 @@ sam deploy \
   --region $REGION \
   --capabilities CAPABILITY_IAM \
   --resolve-s3 \
-  --confirm-changeset
+  --no-confirm-changeset
 
 echo "=== GET OUTPUT TRIAGE ==="
 
@@ -36,6 +39,9 @@ cd ..
 echo "Queue ARN: $QUEUE_ARN"
 echo "API URL: $API_URL"
 
+# ==============================
+# STEP 2 — DISPATCHER
+# ==============================
 echo "=== STEP 2: DEPLOY DISPATCHER ==="
 cd dispatcher
 
@@ -47,21 +53,45 @@ sam deploy \
   --capabilities CAPABILITY_IAM \
   --parameter-overrides QueueArn=$QUEUE_ARN \
   --resolve-s3 \
-  --confirm-changeset
+  --no-confirm-changeset
 
 cd ..
 
+# ==============================
+# STEP 3 — WEB
+# ==============================
 echo "=== STEP 3: DEPLOY WEB ==="
 
 BUCKET="ticketops-prod-web-$RANDOM"
 
 aws s3 mb s3://$BUCKET --region $REGION
 
-# Inject API URL ke HTML
+# ✅ MATIKAN BLOCK PUBLIC ACCESS
+aws s3api put-public-access-block \
+  --bucket $BUCKET \
+  --public-access-block-configuration \
+  BlockPublicAcls=false,IgnorePublicAcls=false,BlockPublicPolicy=false,RestrictPublicBuckets=false
+
+# ✅ SET POLICY PUBLIC
+aws s3api put-bucket-policy \
+  --bucket $BUCKET \
+  --policy "{
+    \"Version\":\"2012-10-17\",
+    \"Statement\":[{
+      \"Effect\":\"Allow\",
+      \"Principal\":\"*\",
+      \"Action\":\"s3:GetObject\",
+      \"Resource\":\"arn:aws:s3:::$BUCKET/*\"
+    }]
+  }"
+
+# ✅ INJECT API URL
 sed "s|API_URL_PLACEHOLDER|$API_URL|g" web/index.html > web/index_deployed.html
 
+# ✅ UPLOAD WEB
 aws s3 cp web/index_deployed.html s3://$BUCKET/index.html
 
+# ✅ ENABLE WEBSITE
 aws s3 website s3://$BUCKET \
   --index-document index.html
 
